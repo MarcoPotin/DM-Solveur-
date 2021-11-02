d426b33f1829bbaa614bf7410f92c2fa53131bcf
MarcoPotin <93533331+MarcoPotin@users.noreply.github.com>
2021-11-02 09:24:57 +0100

    Update solveur.f90

--- solveur.f90
+++ solveur.f90
@@ -1,1 +1,24 @@
+Program DMSolveur
 
+  Use mod_solveur
+  Implicit None
+  Real(Kind=PR),Dimension(:,:),Allocatable :: A,B,C
+  Real(Kind=PR),Dimension(:),Allocatable :: v,x1,x2
+  Integer :: i,n
+  Real(Kind=PR) :: norm1,norm2
+
+  !Instruction
+  n=3
+  A=MatExo1(n)
+  Allocate(v(1:n))
+  Allocate(x1(1:n))
+  Allocate(x2(1:n))
+  v=1._PR
+  Call ResiduMinimum(A,v,x1,1._PR/1000000._PR)
+  Call GPO(A,v,x2,1._PR/1000000._PR)
+  norm1=Norme(v-ProdMatVect(A,x1))
+  norm2=Norme(v-ProdMatVect(A,x2))
+  Print *, norm1, norm2
+  
+
+End Program DMSolveur
